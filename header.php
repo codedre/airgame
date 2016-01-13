7d100cf3726aa4e4c5938cd2d9bbe32bd581f0f8
Matt Popovich <mattpopovich@outlook.com>
2016-01-13 11:09:20 -0500

    NGP form single page template and basic CSS

--- header.php
+++ header.php
@@ -13,16 +13,6 @@ Visit http://airga.me to learn more.
     <?php wp_head(); ?>
   </head>
   <body>
-    <?php wp_nav_menu(); ?>
-    <script type="text/javascript" src="//d1aqhv4sn5kxtx.cloudfront.net/actiontag/at.js"></script>
-    <div class="ngp-form" data-id="<?php
-
-    // Retrieves the stored value from the database
-    $meta_value = get_post_meta( get_the_ID(), 'ngp-data-id-meta-text', true );
-
-    // Checks and displays the retrieved value
-    if( !empty( $meta_value ) ) {
-        echo $meta_value;
-    }
-
-    ?>"></div>
+    <?php if ( get_post_type() !== 'ngp-form-pages' ): ?>
+      <?php wp_nav_menu(); ?>
+    <?php endif; ?>
---
+++ single-ngp-form-pages.php
@@ -0,0 +1,48 @@
+<div id="header">
+  <?php get_header(); ?>
+</div>
+
+<div class="ngp-form-page-body">
+  <div class="ngp-form-page-container">
+    <div class="ngp-form-page-form-sidebar">
+
+      <!--===========<><><> [  Caption Section  ] <><><>===========-->
+
+      <div class="ngp-form-page-caption-box">
+        <?php
+
+        if(have_posts()):
+          while(have_posts()):
+            the_post(); ?>
+              <h2><?php the_title(); ?></h2>
+              <p><?php the_content(); ?></p>
+            <?php endwhile;
+          endif;
+
+          ?>
+        </div>
+
+        <!--===========<><><> [  NGP Form  ] <><><>===========-->
+
+        <div class="ngp-form-page-form-box">
+          <script type="text/javascript" src="//d1aqhv4sn5kxtx.cloudfront.net/actiontag/at.js"></script>
+          <div class="ngp-form" data-id="<?php
+
+          // Retrieves the stored value from the database
+          $meta_value = get_post_meta( get_the_ID(), 'ngp-data-id-meta-text', true );
+
+          // Checks and displays the retrieved value
+          if( !empty( $meta_value ) ) {
+            echo $meta_value;
+          }
+
+          ?>"></div>
+        </div>
+
+      </div> <!-- .ngp-form-page-form-sidebar -->
+  </div> <!-- .ngp-form-page-container -->
+</div> <!-- .ngp-form-page-body -->
+
+<!--===========<><><> [  Footer  ] <><><>=============-->
+
+<?php get_footer(); ?>
