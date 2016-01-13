89b80b5afbec8b3fec170310b0e1affb5475f2b7
Matt Popovich <mattpopovich@outlook.com>
2016-01-13 15:00:52 -0500

    Expands header.php with programmatically generated title, menu, and charset

--- functions.php
+++ functions.php
@@ -42,10 +42,13 @@ function add_scripts(){
 add_action("wp_enqueue_scripts", "add_scripts");
 
 ////////////////////////////////////////////////////////////////////////////////
-//===========<><><> [  Custom meta boxes  ] <><><>============================//
+//===========<><><> [  Menu init  ] <><><>====================================//
 ////////////////////////////////////////////////////////////////////////////////
 
-//nothing here yet
+function airgame_register_theme_menu() {
+    register_nav_menu( 'primary', 'Main Navigation Menu' );
+}
+add_action( 'init', 'airgame_register_theme_menu' );
 
 ////////////////////////////////////////////////////////////////////////////////
 //===========<><><> [  Defaults post structure to postname  ] <><><>==========//
--- header.php
+++ header.php
@@ -8,15 +8,28 @@ Visit http://airga.me to learn more.
 -->
 <html>
   <head>
-    <title><?php bloginfo('name'); ?></title>
-    <meta charset="UTF-8 without BOM" />
+    <title>
+      <?php
+        global $page, $paged;
+        wp_title( '|', true, 'right' );
+
+        //Add the Site Identity campaign name
+        bloginfo( 'name' );
+
+        //Add the Site Identity description on the front-page
+        $site_description = get_bloginfo( 'description', 'display' );
+
+        if ( $site_description && ( is_home() || is_front_page() ) ) {
+          echo " | $site_description";
+        }
+      ?>
+    </title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <?php wp_head(); ?>
   </head>
   <body>
     <?php
-      if ( get_post_type() !== 'ngp-form-pages' ):
-    ?>
-      <?php wp_nav_menu(); ?>
-    <?php
-      endif;
+      if ( get_post_type() !== 'ngp-form-pages' ) {
+        wp_nav_menu( array( 'container_class' => 'main-nav', 'theme_location' => 'primary' ) );
+      }
     ?>
