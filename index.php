137f1262c389f5f2546bc86accf9ce53d2b7225d
Matt Popovich <mattpopovich@outlook.com>
2016-01-15 09:18:42 -0500

    Basic index.php header and styles

--- index.php
+++ index.php
@@ -4,9 +4,18 @@
   <?php get_header(); ?>
 </div>
 
+<div class="airgame-post-header-container">
+  <div class="airgame-post-header-image" style="background-image: url('http://i.imgur.com/z1RFv6j.jpg');";>
+  </div>
+</div>
+
 <!--===========<><><> [  The WP Loop  ] <><><>=============-->
 
-<?php get_template_part("loop"); ?>
+<div class="airgame-content-wrapper">
+  <div class="airgame-content-container">
+    <?php get_template_part("loop"); ?>
+  </div>
+</div>
 
 <!--===========<><><> [  Footer  ] <><><>=============-->
 
