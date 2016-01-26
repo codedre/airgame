2548749f1787ab9042dc1615502c28714e0cae6e
Matt Popovich <mattpopovich@outlook.com>
2016-01-26 14:21:15 -0500

    [see last commit] ...and Header donate ask button linked as well.

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
               <img src="<?php echo get_theme_mod( 'airgame_primary_logo' ); ?>" class="airgame-logo" alt="<?php echo get_bloginfo('name') . " campaign logo"; ?>" />
             </a>
 
-            <a href="<?php echo get_home_url(); ?>/forms/contribute">
+            <a href="<?php echo get_theme_mod( 'airgame_contribute_button_form' ); ?>">
               <div class="airgame-top-donate">
                 <h3 class="airgame_contribute_button_text">
                   <?php echo get_theme_mod( 'airgame_contribute_button_text' ); ?>
