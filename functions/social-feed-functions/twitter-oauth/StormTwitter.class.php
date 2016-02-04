2cde2035b70ae8794a6280c5284b3ef49c36ee65
Matt Popovich <mattpopovich@outlook.com>
2016-02-04 09:25:40 -0500

    Factored out tweet caching temporarily for debugging

--- functions/social-feed-functions/twitter-oauth/StormTwitter.class.php
+++ functions/social-feed-functions/twitter-oauth/StormTwitter.class.php
@@ -1,13 +1,7 @@
 <?php
 
-/*
-* Version 2.1.3
-* The base class for the storm twitter feed for developers.
-* This class provides all the things needed for the wordpress plugin, but in theory means you don't need to use it with wordpress.
-* What could go wrong?
-*/
-
 require_once('oauth/twitteroauth.php');
+
 class StormTwitter {
   private $defaults = array(
     'directory' => '',
@@ -16,21 +10,20 @@ class StormTwitter {
     'token' => '',
     'token_secret' => '',
     'screenname' => '',
-    'cache_expire' => '',
-    'count' => ''
+    'cache_expire' => ''
   );
 
   public $st_last_error = false;
 
-  function __construct($args = array()) {
-    $this->defaults = array_merge($this->defaults, $args);
+  function __construct( $args = array() ) {
+    $this->defaults = array_merge( $this->defaults, $args );
   }
 
   function __toString() {
-    return print_r($this->defaults, true);
+    return print_r( $this->defaults, true );
   }
 
-  function getTweets($screenname = false,$count = 20,$options = false) {
+  function getTweets($screenname = false, $count = 20, $options = false) {
     // BC: $count used to be the first argument
     if (is_int($screenname)) {
       list($screenname, $count) = array($count, $screenname);
@@ -39,24 +32,28 @@ class StormTwitter {
     if ($count > 20) $count = 20;
     if ($count < 1) $count = 1;
 
-    $default_options = array('trim_user'=>true, 'exclude_replies'=>true, 'include_rts'=>false);
+    $default_options = array(
+      'trim_user'       => true,
+      'exclude_replies' => true,
+      'include_rts'     => false
+    );
 
-    if ($options === false || !is_array($options)) {
+    if ($options === false || !is_array( $options )) {
       $options = $default_options;
     } else {
-      $options = array_merge($default_options, $options);
+      $options = array_merge( $default_options, $options );
     }
 
     if ($screenname === false || $screenname === 20) $screenname = $this->defaults['screenname'];
 
-    $result = $this->checkValidCache($screenname,$options);
-
-    if ($result !== false) {
-      return $this->cropTweets($result,$count);
-    }
+    // $result = $this->checkValidCache($screenname,$options);
+    //
+    // if ($result !== false) {
+    //   return $this->cropTweets($result,$count);
+    // }
 
     //If we're here, we need to load.
-    $result = $this->oauthGetTweets($screenname,$options);
+    $result = $this->oauthGetTweets( $screenname, $options );
 
     if (is_array($result) && isset($result['errors'])) {
       if (is_array($result) && isset($result['errors'][0]) && isset($result['errors'][0]['message'])) {
@@ -64,13 +61,13 @@ class StormTwitter {
       } else {
         $last_error = $result['errors'];
       }
-      return array('error'=>'Twitter said: '.json_encode($last_error));
+      return array( 'error' => 'Twitter said: ' . json_encode( $last_error ));
     } else {
-      if (is_array($result)) {
-        return $this->cropTweets($result,$count);
+      if ( is_array( $result ) ) {
+        return $this->cropTweets( $result, $count );
       } else {
-        $last_error = 'Something went wrong with the twitter request: '.json_encode($result);
-        return array('error'=>$last_error);
+        $last_error = 'Something went wrong with the twitter request: ' . json_encode($result);
+        return array('error' => $last_error);
       }
     }
 
@@ -80,54 +77,54 @@ class StormTwitter {
     return array_slice($result, 0, $count);
   }
 
-  private function getCacheLocation() {
-    return $this->defaults['directory'].'.tweetcache';
-  }
+  // private function getCacheLocation() {
+  //   return $this->defaults['directory'].'.tweetcache';
+  // }
 
   private function getOptionsHash($options) {
     $hash = md5(serialize($options));
     return $hash;
   }
 
-  private function checkValidCache($screenname,$options) {
-    $file = $this->getCacheLocation();
-    if (is_file($file)) {
-      $cache = file_get_contents($file);
-      $cache = @json_decode($cache,true);
-
-      if (!isset($cache)) {
-        unlink($file);
-        return false;
-      }
-
-      // Delete the old cache from the first version, before we added support for multiple usernames
-      if (isset($cache['time'])) {
-        unlink($file);
-        return false;
-      }
-
-      $cachename = $screenname."-".$this->getOptionsHash($options);
-
-      //Check if we have a cache for the user.
-      if (!isset($cache[$cachename])) return false;
-
-      if (!isset($cache[$cachename]['time']) || !isset($cache[$cachename]['tweets'])) {
-        unset($cache[$cachename]);
-        file_put_contents($file,json_encode($cache));
-        return false;
-      }
-
-      if ($cache[$cachename]['time'] < (time() - $this->defaults['cache_expire'])) {
-        $result = $this->oauthGetTweets($screenname,$options);
-        if (!isset($result['errors'])) {
-          return $result;
-        }
-      }
-      return $cache[$cachename]['tweets'];
-    } else {
-      return false;
-    }
-  }
+  // private function checkValidCache($screenname,$options) {
+  //   $file = $this->getCacheLocation();
+  //   if (is_file($file)) {
+  //     $cache = file_get_contents($file);
+  //     $cache = @json_decode($cache,true);
+  //
+  //     if (!isset($cache)) {
+  //       unlink($file);
+  //       return false;
+  //     }
+  //
+  //     // Delete the old cache from the first version, before we added support for multiple usernames
+  //     if (isset($cache['time'])) {
+  //       unlink($file);
+  //       return false;
+  //     }
+  //
+  //     $cachename = $screenname."-".$this->getOptionsHash($options);
+  //
+  //     //Check if we have a cache for the user.
+  //     if (!isset($cache[$cachename])) return false;
+  //
+  //     if (!isset($cache[$cachename]['time']) || !isset($cache[$cachename]['tweets'])) {
+  //       unset($cache[$cachename]);
+  //       file_put_contents($file,json_encode($cache));
+  //       return false;
+  //     }
+  //
+  //     if ($cache[$cachename]['time'] < (time() - $this->defaults['cache_expire'])) {
+  //       $result = $this->oauthGetTweets($screenname,$options);
+  //       if (!isset($result['errors'])) {
+  //         return $result;
+  //       }
+  //     }
+  //     return $cache[$cachename]['tweets'];
+  //   } else {
+  //     return false;
+  //   }
+  // }
 
   private function oauthGetTweets($screenname,$options) {
     $key = $this->defaults['key'];
@@ -148,26 +145,78 @@ class StormTwitter {
     $connection = new TwitterOAuth($key, $secret, $token, $token_secret);
     $result = $connection->get('statuses/user_timeline', $options);
 
-    if (is_file($this->getCacheLocation())) {
-      $cache = json_decode(file_get_contents($this->getCacheLocation()),true);
-    }
-
-    if (!isset($result['errors'])) {
-      $cache[$cachename]['time'] = time();
-      $cache[$cachename]['tweets'] = $result;
-      $file = $this->getCacheLocation();
-      file_put_contents($file, json_encode($cache));
-    } else {
-      if (is_array($results) && isset($result['errors'][0]) && isset($result['errors'][0]['message'])) {
-        $last_error = '['.date('r').'] Twitter error: '.$result['errors'][0]['message'];
-        $this->st_last_error = $last_error;
-      } else {
-        $last_error = '['.date('r').'] Twitter returned an invalid response. It is probably down.';
-        $this->st_last_error = $last_error;
-      }
-    }
+    // if (is_file($this->getCacheLocation())) {
+    //   $cache = json_decode(file_get_contents($this->getCacheLocation()),true);
+    // }
+    //
+    // if (!isset($result['errors'])) {
+    //   $cache[$cachename]['time'] = time();
+    //   $cache[$cachename]['tweets'] = $result;
+    //   $file = $this->getCacheLocation();
+    //   file_put_contents($file, json_encode($cache));
+    // } else {
+    //   if (is_array($results) && isset($result['errors'][0]) && isset($result['errors'][0]['message'])) {
+    //     $last_error = '['.date('r').'] Twitter error: '.$result['errors'][0]['message'];
+    //     $this->st_last_error = $last_error;
+    //   } else {
+    //     $last_error = '['.date('r').'] Twitter returned an invalid response. It is probably down.';
+    //     $this->st_last_error = $last_error;
+    //   }
+    // }
 
     return $result;
 
   }
+
+  // //check if cache needs update
+  // $tp_twitter_plugin_last_cache_time = get_option('tp_twitter_plugin_last_cache_time');
+  // $diff = time() - $tp_twitter_plugin_last_cache_time;
+  // $crt = $instance['cachetime'] * 3600;
+  //
+  // //	yes, it needs update
+  // if($diff >= $crt || empty($tp_twitter_plugin_last_cache_time)){
+  //
+  // if(!require_once('twitteroauth.php')){
+  // 	echo '<strong>'.__('Couldn\'t find twitteroauth.php!','tp_tweets').'</strong>' . $after_widget;
+  // 	return;
+  // }
+  //
+  // function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
+  //   $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
+  //   return $connection;
+  // }
+  //
+  // $connection = getConnectionWithAccessToken($instance['consumerkey'], $instance['consumersecret'], $instance['accesstoken'], $instance['accesstokensecret']);
+  // $tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$instance['username']."&count=10&exclude_replies=".$instance['excludereplies']) or die('Couldn\'t retrieve tweets! Wrong username?');
+  //
+  // if(!empty($tweets->errors)){
+  // 	if($tweets->errors[0]->message == 'Invalid or expired token'){
+  // 		echo '<strong>'.$tweets->errors[0]->message.'!</strong><br />' . __('You\'ll need to regenerate it <a href="https://apps.twitter.com/" target="_blank">here</a>!','tp_tweets') . $after_widget;
+  // 	}else{
+  // 		echo '<strong>'.$tweets->errors[0]->message.'</strong>' . $after_widget;
+  // 	}
+  // 	return;
+  // }
+  //
+  // $tweets_array = array();
+  // for($i = 0;$i <= count($tweets); $i++){
+  // 	if(!empty($tweets[$i])){
+  // 		$tweets_array[$i]['created_at'] = $tweets[$i]->created_at;
+  //
+  // 			//clean tweet text
+  // 			$tweets_array[$i]['text'] = preg_replace('/[\x{10000}-\x{10FFFF}]/u', '', $tweets[$i]->text);
+  //
+  // 		if(!empty($tweets[$i]->id_str)){
+  // 			$tweets_array[$i]['status_id'] = $tweets[$i]->id_str;
+  // 		}
+  // 	}
+  // }
+  //
+	// //save tweets to wp option
+	// update_option('tp_twitter_plugin_tweets',serialize($tweets_array));
+	// update_option('tp_twitter_plugin_last_cache_time',time());
+  //
+	// echo '<!-- twitter cache has been updated! -->';
+//}
+
 }
--- functions/social-feed-functions/twitter-oauth/airgame-twitter-oauth-functions.php
+++ functions/social-feed-functions/twitter-oauth/airgame-twitter-oauth-functions.php
@@ -1,7 +1,9 @@
 <?php
 
-require('StormTwitter.class.php');
+require_once('StormTwitter.class.php');
 
+// Define all the variables! \(:D)/
+$temp_path = WP_CONTENT_DIR . '/themes/campaign-theme/functions/social-feed-functions/twitter-oauth/cache/';
 $temp_key = get_theme_mod( 'airgame_twitter_consumer_key' );
 $temp_secret = get_theme_mod( 'airgame_twitter_consumer_secret' );
 $temp_token = get_theme_mod( 'airgame_twitter_access_token' );
@@ -9,17 +11,18 @@ $temp_token_secret = get_theme_mod( 'airgame_twitter_access_token_secret' );
 $temp_username = get_theme_mod( 'airgame_twitter_username' );
 
 $config = array(
-  'directory' => get_template_directory() . '/functions/social-feed-functions/twitter-oauth/oauth/cache/', //The path used to store the .tweetcache cache file.
+  'directory' => $temp_path, //The path used to store the .tweetcache cache file.
   'key' => $temp_key,
   'secret' => $temp_secret,
   'token' => $temp_token,
   'token_secret' => $temp_token_secret,
   'screenname' => '', //This is now deprecated and you shouldn't define this - but it's here for backwards compatibility
-  'cache_expire' => 3600, //The duration of the cache
-  'count' => 4
+  'cache_expire' => 3600 //The duration of the cache
 );
 
 $twitter = new StormTwitter($config);
+
+
 
 // getTweets is the only public method. For legacy reasons, it takes between 0 and 3 parameters.
 // getTweets(twitter_screenname, number_of_tweets, custom_parameters_to_go_twitter);
